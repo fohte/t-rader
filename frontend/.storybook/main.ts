61844c215bcab47c0f81c78a5b60ef7a04158c50
Fohte / Hayato Kawai <fohte.hk@gmail.com>
2026-08-21 00:49:20 +0900

    repo: copier テンプレートを v0.10.2 に更新する (#316)

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
+  staticDirs: ['../public'],
   framework: '@storybook/react-vite',
   addons: [
     '@storybook/addon-docs',
